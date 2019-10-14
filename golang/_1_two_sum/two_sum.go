e8fd78cdac02c9a894f6a38546d8e2b45fc07982
xiabingyao <xiabingyao@tencent.com>
2019-10-14 20:52:49 +0800

    add 1_two_sum

---
+++ golang/_1_two_sum/two_sum.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+)
+
+/*
+解题思路: 减少算法复杂度，不用两层遍历
+*/
+
+func twoSum(nums []int, target int) []int {
+    indexSlice := make([]int, 0)
+    mapSlice := make(map[int]int)
+    for i:=0; i<len(nums); i++ {
+        temp := target - nums[i]
+        if temp < 0 {
+            return indexSlice
+        }
+        if index, ok := mapSlice[temp]; ok && i != index {
+            indexSlice = append(indexSlice, i, index)
+            return indexSlice
+        }
+        mapSlice[nums[i]] = i
+    }
+    return indexSlice
+}
+
+func main() {
+    fmt.Println(twoSum([]int{1, 2},1))
+}
---
+++ golang/_1_two_sum/two_sum_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "reflect"
+    "testing"
+)
+
+func TestTwoSum(t *testing.T) {
+
+    varStrSlice := []struct{
+        intSlice []int
+        target int
+        res []int
+        res2 []int
+    }{
+        {
+            []int{2,7,11,15},
+            9,
+            []int{0, 1},
+            []int{1, 0},
+        },
+        {
+            []int{1,1},
+            2,
+            []int{0, 1},
+            []int{1, 0},
+        },
+        {
+            []int{1,2},
+            2,
+            []int{0, 1},
+            []int{1, 0},
+        },
+        {
+            []int{2},
+            1,
+            []int{0},
+            []int{1},
+        },
+    }
+
+    for _, str := range varStrSlice {
+        result := twoSum(str.intSlice, str.target)
+        if reflect.DeepEqual(result, str.res) {
+            t.Logf("success")
+        } else if reflect.DeepEqual(result, str.res2) {
+            t.Logf("success")
+        } else {
+            t.Logf("failed")
+        }
+    }
+}
