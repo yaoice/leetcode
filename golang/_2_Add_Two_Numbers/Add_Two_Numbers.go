c677c281f446839e63c01a68285216072c524531
iceyao <yao3690093@gmail.com>
2019-10-16 08:12:47 +0800

    add _3_Longest_Substring_Without_Repeating_Characters

--- golang/_2_Add_Two_Numbers/Add_Two_Numbers.go
+++ golang/_2_Add_Two_Numbers/Add_Two_Numbers.go
@@ -1,13 +1,11 @@
 package main
 
-import "fmt"
-
 /*
-2->4->3  243
-5->6->4  564
-7->0->8  807
+2->4->3  245 545
+5->6->4  565 565
+7->0->8  807 1110
 
-递归
+解题思路：递归
 */
 
 type ListNode struct {
@@ -24,6 +22,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
         v, n = add(l1, l2, n)
         temp.Val = v
         l1, l2 = next(l1), next(l2)
+        // 递归终止条件
         if l1 == nil && l2 == nil {
             break
         }
@@ -36,7 +35,6 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
     return result
 }
 
-// next 进入l的下一位。
 func next(l *ListNode) *ListNode {
     if l != nil {
         return l.Next
@@ -51,45 +49,10 @@ func add(n1, n2 *ListNode, i int) (v, n int) {
     if n2 != nil {
         v = v + n2.Val
     }
-
     v = v + i
-
     if v >= 10 {
         v = v - 10
         n = 1
     }
     return v, n
-}
-
-func main() {
-    l1 := &ListNode{
-        Val:  3,
-        Next: &ListNode{
-            Val:  4,
-            Next: &ListNode{
-                Val:  2,
-                Next: nil,
-            },
-        },
-    }
-
-    l2 := &ListNode{
-        Val:  4,
-        Next: &ListNode{
-            Val:  6,
-            Next: &ListNode{
-                Val:  5,
-                Next: nil,
-            },
-        },
-    }
-
-    c := addTwoNumbers(l1, l2)
-    for {
-        fmt.Println(c.Val)
-        if c.Next == nil {
-            break
-        }
-        c = c.Next
-    }
 }
--- golang/_2_Add_Two_Numbers/Add_Two_Numbers_test.go
+++ golang/_2_Add_Two_Numbers/Add_Two_Numbers_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	l1 := &ListNode{
+		Val:  5,
+		Next: &ListNode{
+			Val:  4,
+			Next: &ListNode{
+				Val:  5,
+				Next: nil,
+			},
+		},
+	}
+
+	l2 := &ListNode{
+		Val:  5,
+		Next: &ListNode{
+			Val:  6,
+			Next: &ListNode{
+				Val:  5,
+				Next: nil,
+			},
+		},
+	}
+	c := addTwoNumbers(l1, l2)
+	for {
+		fmt.Println(c.Val)
+		if c.Next == nil {
+			break
+		}
+		c = c.Next
+	}
+}
