47b7e3454058d888df518bc321f08e1121b5ab85
xiabingyao <xiabingyao@tencent.com>
2019-11-04 00:23:21 +0800

    Longest_Substring

--- golang/_3_Longest_Substring_Without_Repeating_Characters/Longest_Substring.go
+++ golang/_3_Longest_Substring_Without_Repeating_Characters/Longest_Substring.go
@@ -1,5 +1,39 @@
 package main
 
+/*
+str: abcdea，统计两个重复字符之间的距离，对比取最大值
+
+解题思路: 动态规划-滑动窗口
+*/
+
 func lengthOfLongestSubstring(s string) int {
-	
+    flag := [256]int{}
+    beg := 0
+    max := 0
+    for i := 0; i < len(s); i++ {
+        if flag[s[i]] > 0 && flag[s[i]] > beg {
+            beg = flag[s[i]]
+        }
+        flag[s[i]] = i+1
+        max = maxnum(max, i - beg + 1)
+    }
+    return max
 }
+
+func maxnum(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+
+/*
+func main() {
+    fmt.Println(lengthOfLongestSubstring("abcdaed"))
+}
+ */
+
+
+
+
--- golang/_3_Longest_Substring_Without_Repeating_Characters/Longest_Substring_test.go
+++ golang/_3_Longest_Substring_Without_Repeating_Characters/Longest_Substring_test.go
@@ -1,1 +1,51 @@
 package main
+
+import "testing"
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+    varStrSlice := []struct{
+        s string
+        res int
+    }{
+        {
+            "abcabcbb",
+            3,
+        },
+        {
+            "bbb",
+            1,
+        },
+        {
+          " ",
+          1,
+        },
+        {
+          "abcde",
+          5,
+        },
+        {
+            "pwwkew",
+            3,
+        },
+        {
+          "",
+          0,
+        },
+        {
+            "aab",
+            2,
+        },
+        {
+            "cdd",
+            2,
+        },
+    }
+
+    for _, str := range varStrSlice {
+        if lengthOfLongestSubstring(str.s) == str.res {
+            t.Log("success")
+        } else {
+            t.Log("failed")
+        }
+    }
+}
